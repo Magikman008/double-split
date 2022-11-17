805ab49f678ac0e833fa944a3c0bdddc2fc58afb
Andrew Dubov <Dubok008@yandex.ru>
2022-11-17 21:08:16 +0300

    themes modified, L added

--- anim.js
+++ anim.js
@@ -3,27 +3,21 @@ var refresh = window.setInterval(add, 1000 / document.getElementById("range").va
 
 clearInterval(refresh);
 
-var color = "black"
+var line_color = "black";
+var dot_color = "white";
+var dot_stroke = "black";
 
 function styleLine(line) {
     line.fill = "transparent";
     line.linewidth = 2;
-    line.stroke = color;
+    line.stroke = line_color;
 }
 
 var two = new Two({ fitted: true }).appendTo(elem);
 
 var centerY = 350;
 
-
 var serifs = Array();
-function make_serifs() {
-    styleLine(linewhole);
-
-    for (var i = 0; i <= 40; i++) {
-        styleLine(serifs[i]);
-    }
-}
 
 var lineup = two.makeLine(200, 0, 200, centerY - 40 + distance / 3);
 var linecenter = two.makeLine(200, centerY - 20 + distance / 3, 200, centerY + 20 - distance / 3);
@@ -51,11 +45,11 @@ two
             if (dots[j].position.x < 200) {
                 if (holes[j] == 1) {
                     dots[j].position.x += 5;
-                    dots[j].position.y = 350 + (15 - distance / 6) * Math.sin(Math.PI * ((dots[j].position.x - 50) / 150 - 1 / 2)) + 15 - distance / 6;
+                    dots[j].position.y = 350 + (15 - distance / 6) * Math.sin(Math.PI * ((dots[j].position.x - 50 - L) / (150 - L) - 1 / 2)) + 15 - distance / 6;
                 }
                 else {
                     dots[j].position.x += 5;
-                    dots[j].position.y = 350 - (15 - distance / 6) * Math.sin(Math.PI * ((dots[j].position.x - 50) / 150 - 1 / 2)) - 15 + distance / 6;
+                    dots[j].position.y = 350 - (15 - distance / 6) * Math.sin(Math.PI * ((dots[j].position.x - 50 - L) / (150 - L) - 1 / 2)) - 15 + distance / 6;
                 }
             }
             else if (dots[j].position.x < dict_poins[String(j)][0] + 850 - y) {
@@ -99,7 +93,9 @@ var L = 5;
 var distance = 10;
 
 function add() {
-    dots.push(two.makeCircle(50, centerY, 2));
+    dots.push(two.makeCircle(50 + L, centerY, 3));
+    dots[i].fill = dot_color;
+    dots[i].stroke = dot_stroke;
     var slot = Math.round(1 - 0.5 + Math.random() * (2));
     if (slot == 1)
         cords.push(interfernce_pattern_generator(1, x - distance, i));
@@ -153,15 +149,37 @@ document.getElementById("range4").oninput = function () {
     L = Number(this.value);
 }
 
+function make_serifs() {
+    styleLine(linewhole);
+
+    for (var i = 0; i <= 40; i++) {
+        styleLine(serifs[i]);
+    }
+}
+
 const theme = document.querySelector("#theme-link");
 document.getElementById("Theme").addEventListener("click", function () {
     if (theme.getAttribute("href") == "light-theme.css") {
         theme.href = "dark-theme.css";
-        color = "white";
+        line_color = "white";
+        dot_color = "black";
+        dot_stroke = "white";
         make_serifs();
+
+        for (var j = 0; j < i; j++) {
+            dots[j].fill = dot_color;
+            dots[j].stroke = dot_stroke;
+        }
     } else {
         theme.href = "light-theme.css";
-        color = "black";
+        line_color = "black";
+        dot_color = "white";
+        dot_stroke = "black";
         make_serifs();
+
+        for (var j = 0; j < i; j++) {
+            dots[j].fill = dot_color;
+            dots[j].stroke = dot_stroke;
+        }
     }
 });
